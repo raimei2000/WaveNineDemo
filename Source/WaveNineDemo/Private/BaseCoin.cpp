ac7cb1127cd07cceb4256b7aa66d19c3da656b21
raimei <gigae0108@gmail.com>
2026-08-19 17:04:56 +0900

    feat: coin

---
+++ Source/WaveNineDemo/Private/BaseCoin.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCoin.h"
+
+ABaseCoin::ABaseCoin()
+{
+    PointValue = 0;
+    ItemType = "DefaultCoin";
+}
+
+void ABaseCoin::ActivateItem(AActor* Activator)
+{
+    if (Activator && Activator->ActorHasTag("Player"))
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Black, FString::Printf(TEXT("Coin")));
+        DestroyItem();
+    }
+}
