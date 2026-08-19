d050b9d11d4236224ccc369f4bfac2b579df1b42
raimei <gigae0108@gmail.com>
2026-08-19 15:42:17 +0900

    feat: base item

---
+++ Source/WaveNineDemo/Public/BaseItem.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemInterface.h"
+#include "GameFramework/Actor.h"
+#include "BaseItem.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class WAVENINEDEMO_API ABaseItem : public AActor, public IItemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	ABaseItem();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemType;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USceneComponent* Scene;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	UStaticMeshComponent* StaticMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item|Component")
+	USphereComponent* Collision;
+
+	virtual void OnItemOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult) override;
+	virtual void OnItemEndOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex) override;
+	virtual void ActivateItem(AActor* Activator) override;
+	virtual FName GetItemType() const override;
+
+};
