4a0f1f554ad7cc38269821454e734c3b38fb4100
raimei <gigae0108@gmail.com>
2026-08-20 17:18:40 +0900

    feat: health potion

---
+++ Source/WaveNineDemo/Private/HealthPotionItem.cpp
@@ -0,0 +1,15 @@
+﻿#include "HealthPotionItem.h"
+
+AHealthPotionItem::AHealthPotionItem()
+{
+    HealAmount = 30.f;
+    ItemType = "HealthPotion";
+}
+
+void AHealthPotionItem::ActivateItem(AActor* Activator)
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Green, FString(TEXT("Heal")));
+    }
+}
---
+++ Source/WaveNineDemo/Public/HealthPotionItem.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseItem.h"
+#include "HealthPotionItem.generated.h"
+
+UCLASS()
+class WAVENINEDEMO_API AHealthPotionItem : public ABaseItem
+{
+	GENERATED_BODY()
+
+public:
+	AHealthPotionItem();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Heal")
+	float HealAmount;
+
+	virtual void ActivateItem(AActor* Activator) override;
+	
+};
