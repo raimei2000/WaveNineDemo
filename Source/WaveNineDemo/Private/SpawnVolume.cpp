0884b6122fda0499a1cb60613242622126107bbf
raimei <gigae0108@gmail.com>
2026-08-21 11:15:18 +0900

    feat: create spawn volume

---
+++ Source/WaveNineDemo/Private/SpawnVolume.cpp
@@ -0,0 +1,38 @@
+﻿#include "SpawnVolume.h"
+#include "Components/BoxComponent.h"
+
+ASpawnVolume::ASpawnVolume()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	SetRootComponent(RootScene);
+
+	SpawnVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnVolume"));
+	SpawnVolume->SetupAttachment(RootScene);
+
+	SizeX = 0.f;
+	SizeY = 0.f;
+	SizeZ = 0.f;
+	OriginLocation = FVector(0.f, 0.f, 0.f);
+}
+
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FVector BoxSize = SpawnVolume->GetScaledBoxExtent();
+	SizeX = BoxSize.X;
+	SizeY = BoxSize.Y;
+	SizeZ = BoxSize.Z;
+
+	OriginLocation = GetActorLocation();
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume()
+{
+	return FVector(
+		FMath::FRandRange(OriginLocation.X - SizeX, OriginLocation.X + SizeX),
+		FMath::FRandRange(OriginLocation.Y - SizeY, OriginLocation.Y + SizeY),
+		FMath::FRandRange(OriginLocation.Z - SizeZ, OriginLocation.Z + SizeZ));
+}
---
+++ Source/WaveNineDemo/Public/SpawnVolume.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+class UBoxComponent;
+UCLASS()
+class WAVENINEDEMO_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASpawnVolume();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* RootScene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UBoxComponent* SpawnVolume;
+
+protected:
+	virtual void BeginPlay() override;
+
+	FVector GetRandomPointInVolume();
+
+private:
+	FVector OriginLocation;
+	float SizeX = 0.f;
+	float SizeY = 0.f;
+	float SizeZ = 0.f;
+};
