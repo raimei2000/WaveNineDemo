bc50109cd89abeefabd9f1c67de05fc0945812b8
raimei <gigae0108@gmail.com>
2026-08-21 16:19:56 +0900

    feat: create game instance

---
+++ Source/WaveNineDemo/Private/NineGameInstance.cpp
@@ -0,0 +1,27 @@
+﻿#include "NineGameInstance.h"
+
+UNineGameInstance::UNineGameInstance()
+{
+    TotalScore = 0;
+    CurrentLevelIndex = 0;
+}
+
+void UNineGameInstance::AddTotalScore(int32 Amount)
+{
+    TotalScore += Amount;
+}
+
+int32 UNineGameInstance::GetTotalScore()
+{
+    return TotalScore;
+}
+
+int32 UNineGameInstance::GetCurrentLevelIndex()
+{
+    return CurrentLevelIndex;
+}
+
+void UNineGameInstance::IncreaseLevelIndex()
+{
+    CurrentLevelIndex++;
+}
---
+++ Source/WaveNineDemo/Public/NineGameInstance.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "NineGameInstance.generated.h"
+
+UCLASS()
+class WAVENINEDEMO_API UNineGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UNineGameInstance();
+
+	void AddTotalScore(int32 Amount);
+
+	int32 GetTotalScore();
+
+	int32 GetCurrentLevelIndex();
+
+	void IncreaseLevelIndex();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameDate")
+	int32 TotalScore;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameDate")
+	int32 CurrentLevelIndex;
+};
