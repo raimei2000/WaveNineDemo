5e0cc937acf98ccee7947eb74e5567bbb8ae86b3
raimei <gigae0108@gmail.com>
2026-08-25 12:27:05 +0900

    feat: mine spec data

---
+++ Source/WaveNineDemo/Public/MineSpecRow.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MineSpecRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMineSpecRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<float> DamagePerWave;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<float> ExplosionDelayPerWave;
+};
