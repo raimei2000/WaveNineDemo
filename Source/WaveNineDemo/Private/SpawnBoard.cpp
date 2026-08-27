310f52ef25c8b95bb3cf1139300fa232701a0923
raimei <gigae0108@gmail.com>
2026-08-27 19:18:19 +0900

    feat: spawn spike at random location

---
+++ Source/WaveNineDemo/Private/SpawnBoard.cpp
@@ -0,0 +1,43 @@
+﻿#include "SpawnBoard.h"
+#include "Trap.h"
+#include "Components/BoxComponent.h"
+
+ASpawnBoard::ASpawnBoard()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(RootScene);
+
+	SpawnBoard = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnBoard"));
+	SpawnBoard->SetupAttachment(RootScene);
+
+	SpawnDelay = 2.5f;
+}
+
+void ASpawnBoard::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SizeX = SpawnBoard->GetScaledBoxExtent().X;
+	SizeY = SpawnBoard->GetScaledBoxExtent().Y;
+	OriginLocation = GetActorLocation();
+
+	GetWorldTimerManager().SetTimer(SpawnSpikeTimerHandle, this, &ASpawnBoard::SpawnTrap, SpawnDelay, true);
+}
+
+FVector2D ASpawnBoard::GetRandomPointOnBoard() const
+{
+	return FVector2D(
+		FMath::FRandRange(OriginLocation.X - SizeX, OriginLocation.X + SizeX),
+		FMath::FRandRange(OriginLocation.Y - SizeY, OriginLocation.Y + SizeY));
+}
+
+void ASpawnBoard::SpawnTrap() const
+{
+	if (ActorToSpawn)
+	{
+		FVector2D Rand2DPoint = GetRandomPointOnBoard();
+		GetWorld()->SpawnActor<ATrap>(ActorToSpawn, FVector(Rand2DPoint.X, Rand2DPoint.Y, -10.f), FRotator::ZeroRotator);
+	}
+}
---
+++ Source/WaveNineDemo/Public/SpawnBoard.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnBoard.generated.h"
+
+class UBoxComponent;
+class ATrap;
+
+UCLASS()
+class WAVENINEDEMO_API ASpawnBoard : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASpawnBoard();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* RootScene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UBoxComponent* SpawnBoard;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn")
+	TSubclassOf<ATrap> ActorToSpawn;
+
+private:
+	FVector2D GetRandomPointOnBoard() const;
+
+	void SpawnTrap() const;
+
+private:
+	FVector OriginLocation;
+	float SizeX = 0.f;
+	float SizeY = 0.f;
+
+	FTimerHandle SpawnSpikeTimerHandle;
+	float SpawnDelay;
+};
