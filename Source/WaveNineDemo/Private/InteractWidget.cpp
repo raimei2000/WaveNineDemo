6e25626bb40a5b0c164ca3e0f1b2e5e1d4e0f04a
raimei <gigae0108@gmail.com>
2026-08-31 17:53:28 +0900

    feat: add UInteractWidget

---
+++ Source/WaveNineDemo/Private/InteractWidget.cpp
@@ -0,0 +1,41 @@
+﻿#include "InteractWidget.h"
+
+void UInteractWidget::OnHideAnimFinished()
+{
+    if (bHide)
+    {
+        SetVisibility(ESlateVisibility::Collapsed);
+    }
+}
+
+void UInteractWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (ShowAnim)
+    {
+        FWidgetAnimationDynamicEvent EndDelegate;
+        EndDelegate.BindDynamic(this, &UInteractWidget::OnHideAnimFinished);
+        BindToAnimationFinished(ShowAnim, EndDelegate);
+    }
+}
+
+void UInteractWidget::PlayShow()
+{
+    SetVisibility(ESlateVisibility::HitTestInvisible);
+
+    if (ShowAnim)
+    {
+        bHide = false;
+        PlayAnimation(ShowAnim, 0.f, 1, EUMGSequencePlayMode::Forward, 0.f);
+    }
+}
+
+void UInteractWidget::PlayHide()
+{
+    if (ShowAnim)
+    {
+        bHide = true;
+        PlayAnimation(ShowAnim, 0.f, 1, EUMGSequencePlayMode::Reverse, 0.f);
+    }
+}
---
+++ Source/WaveNineDemo/Public/InteractWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InteractWidget.generated.h"
+
+UCLASS()
+class WAVENINEDEMO_API UInteractWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidgetAnim), Transient)
+	TObjectPtr<UWidgetAnimation> ShowAnim;
+
+	UFUNCTION()
+	void OnHideAnimFinished();
+
+	virtual void NativeConstruct() override;
+
+	void PlayShow();
+
+	void PlayHide();
+
+private:
+	bool bHide;
+};
